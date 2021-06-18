a35dc7b933450f0d7ce826dae7bb9ab1e6a34675
MAZAKPE <makpalyy@gmail.com>
2021-06-18 11:25:53 +0300

    add image assets

--- app/src/main/java/com/maku/pombe/data/remote/api/CocktailsApi.kt
+++ app/src/main/java/com/maku/pombe/data/remote/api/CocktailsApi.kt
@@ -1,5 +1,6 @@
 package com.maku.pombe.data.remote.api
 
+import com.maku.pombe.BuildConfig
 import com.maku.pombe.data.models.latest.Latest
 import com.maku.pombe.data.models.popular.Popular
 import com.maku.pombe.data.models.recent.Recent
@@ -9,17 +10,17 @@ import retrofit2.http.GET
 interface CocktailsApi {
 
     // get recent cocktails
-    @GET("/api/json/v2/9973533/recent.php")
+    @GET("/api/json/v2/${BuildConfig.API_KEY}/recent.php")
     suspend fun getRecentCocktail(
     ): Response<Recent>
 
     // get popular cocktails 9973533
-    @GET("/api/json/v2/9973533/popular.php")
+    @GET("/api/json/v2/${BuildConfig.API_KEY}/popular.php")
     suspend fun getPopularCocktail(
     ): Response<Popular>
 
     // get latest cocktails 9973533
-    @GET("/api/json/v2/9973533/latest.php")
+    @GET("/api/json/v2/${BuildConfig.API_KEY}/latest.php")
     suspend fun getLatestsCocktail(
     ): Response<Latest>
 }
