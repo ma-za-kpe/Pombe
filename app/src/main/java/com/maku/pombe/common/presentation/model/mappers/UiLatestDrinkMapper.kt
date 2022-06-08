41f268ec46efb03d24ebb21201e406359ea7ce87
MAZAKPE <makpalyy@gmail.com>
2022-06-08 21:28:45 +0300

    fix presentaion model mapper issue

--- app/src/main/java/com/maku/pombe/common/presentation/model/mappers/UiLatestDrinkMapper.kt
+++ app/src/main/java/com/maku/pombe/common/presentation/model/mappers/UiLatestDrinkMapper.kt
@@ -1,13 +1,7 @@
 package com.maku.pombe.common.presentation.model.mappers
 
-import com.maku.pombe.common.data.cache.model.cachedLatest.CachedLatest
 import com.maku.pombe.common.domain.model.latest.LatestDrink
-import com.maku.pombe.common.domain.model.popular.PopularDrink
-import com.maku.pombe.common.domain.model.shared.Details
 import com.maku.pombe.common.presentation.model.latest.UILatestDrink
-import com.maku.pombe.common.presentation.model.latest.UILatestDrinkDetails
-import com.maku.pombe.common.presentation.model.popular.UIPopularDrink
-import com.maku.pombe.common.presentation.model.popular.UIPopularDrinkDetails
 import javax.inject.Inject
 
 class UiLatestDrinkMapper @Inject constructor(): UiMapper<LatestDrink, UILatestDrink> {
@@ -23,29 +17,3 @@ class UiLatestDrinkMapper @Inject constructor(): UiMapper<LatestDrink, UILatestD
   }
 }
 
-class UiLatestDrinkDetailsMapper @Inject constructor(): UiMapper<CachedLatest, UILatestDrinkDetails> {
-  override fun mapToView(input: CachedLatest): UILatestDrinkDetails {
-    return UILatestDrinkDetails(
-      id = input.idDrink,
-      name = input.strDrink,
-      alcoholic = input.strAlcoholic,
-      category = input.strCategory,
-      photo = input.strDrinkThumb,
-     strCreativeCommonsConfirmed = input.strCreativeCommonsConfirmed,
-     strDrink = input.strDrink,
-     strDrinkAlternate = input.strDrinkAlternate,
-     strDrinkThumb = input.strDrinkThumb,
-     strGlass = input.strGlass,
-     strIBA = input.strIBA,
-     strImageAttribution = input.strImageAttribution,
-     strImageSource = input.strImageSource,
-     strTags = input.strTags,
-     strVideo = input.strVideo,
-    details = Details(
-      input.ingredients,
-      input.instructions,
-      input.measures
-    ),
-    )
-  }
-}
